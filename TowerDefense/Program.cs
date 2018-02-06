b181470c772982731ffb04fe4f6d3d6eceeccc4b
Brenden Seidel <brenseid@gmail.com>
2018-02-06 10:01:48 -0500

    instantiate map class in program

--- TowerDefense/Program.cs
+++ TowerDefense/Program.cs
@@ -6,7 +6,9 @@ namespace TowerDefense
     {
         static void Main(string[] args)
         {
-            Tower tower = new Tower();
+            Map map = new Map(8, 5);
+
+            int area = map.Width + map.Height;
         }
     }
 }
