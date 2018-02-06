4c0d5864a2a3babb88c8c723ed0f3fc67c5e99d5
Brenden Seidel <brenseid@gmail.com>
2018-02-06 11:42:37 -0500

    add exception class

---
+++ TowerDefense/Exceptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefense
+{
+    class TowerDefenseException : Exception
+    {
+        public TowerDefenseException()
+        {
+
+        }
+        public TowerDefenseException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : TowerDefenseException
+    {
+        public OutOfBoundsException()
+        {
+
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+}
