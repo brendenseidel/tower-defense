e71287722d297465942744bbaec208086d31bb5e
Brenden Seidel <brenseid@gmail.com>
2018-02-06 15:44:30 -0500

    create tower constructor

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -6,6 +6,15 @@ namespace TowerDefense
 {
     class Tower
     {
+        private readonly MapLocation _location;
 
+        public Tower(MapLocation location)
+        {
+            if (!location.OnMap(this))
+            {
+                throw new OutOfBoundsException(x + ", " + y + " is outside the boundaries of the map.");
+            }
+            _location = location;
+        }
     }
 }
